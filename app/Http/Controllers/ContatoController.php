954bda873977579c2ec5a90021dbd3256de6a640
edumcarvalho <dudumcarvalho@gmail.com>
2022-12-12 16:20:19 -0300

    Aula 65

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,6 +8,7 @@ class ContatoController extends Controller
 {
     public function contato() //isso pode ser conhecido como uma action
     {
+        var_dump($_GET);
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
