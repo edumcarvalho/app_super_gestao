2b1240fdfd5291d605088e9fb58c6b42be489e52
edumcarvalho <dudumcarvalho@gmail.com>
2022-12-12 21:45:25 -0300

    Aula 72

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,7 +8,7 @@ class ContatoController extends Controller
 {
     public function contato() //isso pode ser conhecido como uma action
     {
-        var_dump($_GET);
+        var_dump($_POST);
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,7 +1,6 @@
 @extends('site.layouts.basico')
 
-@section('titulo', $titulo)
-    
+@section('titulo', $titulo)    
 
 @section('conteudo')
 
@@ -14,24 +13,10 @@
 
         <div class="informacao-pagina">
             <div class="contato-principal">
-                <form action={{ route('site.contato' )}} method="GET">
-                    <input name="nome" type="text" placeholder="Nome" class="borda-preta">
-                    <br>
-                    <input name="telefone" type="text" placeholder="Telefone" class="borda-preta">
-                    <br>
-                    <input name="email" type="text" placeholder="E-mail" class="borda-preta">
-                    <br>
-                    <select name="motivo_contato" class="borda-preta">
-                        <option value="">Qual o motivo do contato?</option>
-                        <option value="">Dúvida</option>
-                        <option value="">Elogio</option>
-                        <option value="">Reclamação</option>
-                    </select>
-                    <br>
-                    <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                    <br>
-                    <button type="submit" class="borda-preta">ENVIAR</button>
-                </form>
+                @component('site.layouts._components.form_contato', ['classe'=>'borda-preta'])
+                    <p>Esta é uma maneira de passar parametro para a view dentro de um component</p>
+                    
+                @endcomponent
             </div>
         </div>  
     </div>
