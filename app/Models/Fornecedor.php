a79d3b0713aab8dbee0e91e05919e5fae7d6f399
edumcarvalho <dudumcarvalho@gmail.com>
2022-12-14 23:35:19 -0300

    foi

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    use HasFactory;
+    protected $table = 'fornecedores';
+
+    protected $fillable = ['nome', 'site', 'email'];
 }
